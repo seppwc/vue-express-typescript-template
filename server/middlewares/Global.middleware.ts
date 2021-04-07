f299745a3577db9a8c5467b1a8dcf3f7a45bce2e
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2021-04-07 16:20:36 +0100

    fomatting

--- server/middlewares/Global.middleware.ts
+++ server/middlewares/Global.middleware.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import { json, urlencoded } from "express";
 import morgan from "morgan";
 
-
 export const GlobalMiddleWares = [
   json(), // parse json data
   urlencoded({ extended: false }), // parse urlencoded bodies
